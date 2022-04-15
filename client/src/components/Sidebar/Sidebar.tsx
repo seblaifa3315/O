21c245a5ed4529f141b05f44e0fbbeea5532c58d
Sebastien Laifa <seblaifa@hotmail.com>
2022-04-14 17:49:38 -0700

    ...

--- client/src/components/Sidebar/Sidebar.tsx
+++ client/src/components/Sidebar/Sidebar.tsx
@@ -1,27 +1,40 @@
 import React from "react";
 import useStyles from "./useStyles";
-import { Box, Typography } from "@mui/material";
-import { LineStyle, Timeline, Person, TrendingUp, PermIdentity, Storefront, AttachMoney, BarChart, MailOutline, DynamicFeed, ChatBubbleOutline, WorkOutline, Report } from "@mui/icons-material";
+import { Box, Typography, Divider, Grid, Stack } from "@mui/material";
+import { Timeline, Person, Add } from "@mui/icons-material";
 import { NavLink } from "react-router-dom";
+import { useAuth } from "../../context/useAuthContext";
+import AddCircleRoundedIcon from "@mui/icons-material/AddCircleRounded";
+import GroupsIcon from "@mui/icons-material/Groups";
 
 export default function Sidebar() {
     const classes = useStyles();
+    const { loggedInUser, profile } = useAuth();
     return (
         <Box className={classes.sidebarContainer}>
-            <Box>
-                <NavLink to="/divers" className="link">
+            <NavLink to="/divers" className="link">
+                <Stack className={classes.item}>
+                    <GroupsIcon />
+                    {/* <Typography variant="subtitle1">Divers</Typography> */}
+                </Stack>
+            </NavLink>
+
+            <NavLink to="/analytics" className="link">
+                <Stack className={classes.item}>
+                    <Timeline />
+                    {/* <Typography variant="subtitle1">Analytics</Typography> */}
+                </Stack>
+            </NavLink>
+
+            {loggedInUser && loggedInUser.isAdmin && (
+                <NavLink to="/register" className="link">
                     <Box className={classes.item}>
-                        <Person sx={{ marginRight: "5px" }} />
-                        <Typography variant="subtitle1">Divers</Typography>
+                        <AddCircleRoundedIcon />
+                        {/* <Typography variant="subtitle1">Add a diver</Typography> */}
                     </Box>
-                    <NavLink to="/analytics" className="link">
-                        <Box className={classes.item}>
-                            <Timeline sx={{ marginRight: "5px" }} />
-                            <Typography variant="subtitle1">Analytics</Typography>
-                        </Box>
-                    </NavLink>
                 </NavLink>
-            </Box>
+            )}
         </Box>
     );
 }
+
